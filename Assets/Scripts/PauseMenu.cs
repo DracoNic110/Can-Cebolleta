97e7db83e6d012cc473be6b4876578bb6f989b67
JhonAlex1234 <jhona8808@gmail.com>
2025-10-13 19:06:28 +0200

    CommitGod

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,8 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+<<<<<<< Updated upstream
     [SerializeField] GameObject pauseMenu; 
 
     public void Pause()
@@ -12,6 +15,32 @@ public class PauseMenu : MonoBehaviour
     }
     public void Resume()
     {
+=======
+    bool gamePaused = false;
+    [SerializeField] GameObject pauseMenu; 
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && gamePaused == false)
+        {
+            gamePaused = true;
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+            
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && gamePaused == true) 
+        {
+            gamePaused = false;
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+            
+        }    
+    }
+    public void Resume()
+    {
+        gamePaused = false;
+>>>>>>> Stashed changes
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
     }
