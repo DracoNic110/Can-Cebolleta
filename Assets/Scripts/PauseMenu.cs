8a0181d4219d56c134e5cafb80302d8c750e816b
JhonAlex1234 <jhona8808@gmail.com>
2025-10-13 18:09:36 +0200

    Revert "el boton si que sirve ahora MCELP"
    This reverts commit 7935f858d205dfe47aa086fa6b7e589d00479c63.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,34 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    bool gamePaused = false;
     [SerializeField] GameObject pauseMenu; 
 
-    public void Update()
+    public void Pause()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && gamePaused == false)
-        {
-            gamePaused = true;
-            pauseMenu.SetActive(true);
-            Time.timeScale = 0;
-            
-
-        }
-        else if (Input.GetKeyDown(KeyCode.Escape) && gamePaused == true) 
-        {
-            gamePaused = false;
-            pauseMenu.SetActive(false);
-            Time.timeScale = 1;
-            
-        }    
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
     }
     public void Resume()
     {
-        gamePaused = false;
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
     }
