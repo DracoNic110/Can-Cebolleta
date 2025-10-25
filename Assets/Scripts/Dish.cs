763d384e9631c86e62942a52dc711e20d9a82900
JhonAlex1234 <jhona8808@gmail.com>
2025-10-25 18:36:22 +0200

    Pathfinding y servir platos completado
    Mañana realizo el sistema del dinero

--- Assets/Scripts/Dish.cs
+++ Assets/Scripts/Dish.cs
@@ -1,9 +1,12 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Dish : MonoBehaviour
 {
     public ClientBehavior clientOwner;
     public Food FoodData;
+
+    [Header("Configuración del restaurante")]
+    public Transform exitPoint;
 
     public void AssignOrder(ClientBehavior client)
     {
@@ -14,5 +17,31 @@ public class Dish : MonoBehaviour
     public void Initialize(Food food)
     {
         FoodData = food;
+    }
+
+    public void StartEatingRoutine()
+    {
+        StartCoroutine(EatingCoroutine());
+    }
+
+    private System.Collections.IEnumerator EatingCoroutine()
+    {
+        float eatTime = Random.Range(10f, 15f);
+        Debug.Log($"{clientOwner.name} está comiendo {FoodData.name} durante {eatTime:F1}s...");
+        yield return new WaitForSeconds(eatTime);
+
+        Debug.Log($"{clientOwner.name} ha terminado de comer y se levanta.");
+
+        if (exitPoint == null)
+        {
+            GameObject exit = GameObject.Find("ClientPoints/ExitPoint");
+            if (exit != null)
+                exitPoint = exit.transform;
+        }
+
+        if (exitPoint != null)
+            clientOwner.LeaveRestaurant(exitPoint.position);
+
+        Destroy(gameObject);
     }
 }
