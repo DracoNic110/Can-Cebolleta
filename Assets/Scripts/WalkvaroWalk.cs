19a98efbaaeef66ef25ae3dd620e27bb6f9895e5
Alvaro <235373871+DracoNic110@users.noreply.github.com>
2025-10-27 13:02:30 +0100

    Walkvaro animation

--- Assets/Scripts/WalkvaroWalk.cs
+++ Assets/Scripts/WalkvaroWalk.cs
@@ -2,15 +2,47 @@ using UnityEngine;
 
 public class WalkvaroWalk : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool moovingRight;
+
+    private Vector3 startPoint;
+
+    private Vector3 endPoint;
+
+    private Vector3 move = new Vector3(1f,0f,0f);
+
+    [SerializeField] float speed;
+
+    [SerializeField] private Animator animator;
+
     void Start()
     {
-        
+        startPoint = new Vector3(-2.55f, 3.367f, 0.0f);
+        endPoint = new Vector3(2.55f, 3.367f, 0.0f);
+        moovingRight = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if (moovingRight)
+        {
+            transform.Translate(move * speed * Time.deltaTime);
+        }
+
+        if (transform.position.x > endPoint.x)
+        {
+            moovingRight = false;
+            animator.SetBool("moovingRight", false);
+            move.x = -1;
+        }
+        if (!moovingRight) {
+            transform.Translate(move * speed * Time.deltaTime);
+        }
+        if (transform.position.x < startPoint.x) {
+            moovingRight = true;
+            animator.SetBool("moovingRight", true);
+            move.x = 1;
+        }
+
     }
 }
